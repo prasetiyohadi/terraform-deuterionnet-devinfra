2b7ecee0922efed5d2e0203ef93f4cae77b389dd
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2021-09-05 19:27:10 +0700

    refactor: update output entries description

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,34 @@
-output "network_id" {
-  value = google_compute_network.vpc_network.id
+output "gcp_project" {
+  description = "The GCP Project where all resources are deployed."
+  value       = var.gcp_project_id
 }
 
-output "network_gateway_ipv4" {
-  value = google_compute_network.vpc_network.gateway_ipv4
+output "gcp_region" {
+  description = "The GCP region where all resources are deployed."
+  value       = var.gcp_region
 }
 
-output "instance_id" {
-  value = google_compute_instance.vm_instance.id
+output "gcp_zone" {
+  description = "The GCP zone where all resources are deployed."
+  value       = google_compute_instance.vm_instance.zone
 }
 
-output "instance_network_ip" {
-  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+output "instance_name" {
+  description = "The name of the compute instance."
+  value       = google_compute_instance.vm_instance.name
 }
 
 output "instance_nat_ip" {
-  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+  description = "The public IP address of the compute instance."
+  value       = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+}
+
+output "instance_network_ip" {
+  description = "The internal IP address of the compute instance."
+  value       = google_compute_instance.vm_instance.network_interface.0.network_ip
+}
+
+output "vpc_name" {
+  description = "The name of the virtual private network (VPC)."
+  value       = google_compute_network.vpc_network.name
 }
